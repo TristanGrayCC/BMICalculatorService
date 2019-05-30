4d8d7554f591834c92197c184e1889f95e958cd7
Tristan Gray <tristangraycc@gmail.com>
2019-05-30 15:06:57 +0100

    Startup refactor

--- BMIApplication/Startup.cs
+++ BMIApplication/Startup.cs
@@ -39,56 +39,8 @@ namespace BMIApplication
                 options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
             })
-    .AddCookie()
-    .AddOpenIdConnect("Auth0", options =>
-    {
-        // Set the authority to your Auth0 domain
-        options.Authority = $"https://{Configuration["Auth0:Domain"]}";
-
-        // Configure the Auth0 Client ID and Client Secret
-        options.ClientId = Configuration["Auth0:ClientId"];
-        options.ClientSecret = Configuration["Auth0:ClientSecret"];
-
-        // Set response type to code
-        options.ResponseType = "code";
-
-        // Configure the scope
-        options.Scope.Clear();
-        options.Scope.Add("openid");
-
-        // Set the callback path, so Auth0 will call back to http://localhost:3000/callback
-        // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
-        options.CallbackPath = new PathString("/callback");
-
-        // Configure the Claims Issuer to be Auth0
-        options.ClaimsIssuer = "Auth0";
-
-        options.Events = new OpenIdConnectEvents
-        {
-            // handle the logout redirection
-            OnRedirectToIdentityProviderForSignOut = (context) =>
-            {
-                var logoutUri = $"https://{Configuration["Auth0:Domain"]}/v2/logout?client_id={Configuration["Auth0:ClientId"]}";
-
-                var postLogoutUri = context.Properties.RedirectUri;
-                if (!string.IsNullOrEmpty(postLogoutUri))
-                {
-                    if (postLogoutUri.StartsWith("/"))
-                    {
-                        // transform to absolute
-                        var request = context.Request;
-                        postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
-                    }
-                    logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
-                }
-
-                context.Response.Redirect(logoutUri);
-                context.HandleResponse();
-
-                return Task.CompletedTask;
-            }
-        };
-    });
+            .AddCookie()
+            .AddOpenIdConnect("Auth0", SetOpenIdConnectOptions);
 
 
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -126,6 +78,42 @@ namespace BMIApplication
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private void SetOpenIdConnectOptions(OpenIdConnectOptions options)
+        {
+            options.Authority = $"https://{Configuration["Auth0:Domain"]}";
+            options.ClientId = Configuration["Auth0:ClientId"];
+            options.ClientSecret = Configuration["Auth0:ClientSecret"];
+            options.ResponseType = "code";
+            options.Scope.Clear();
+            options.Scope.Add("openid");
+            options.CallbackPath = new PathString("/callback");
+            options.ClaimsIssuer = "Auth0";
+
+            options.Events = new OpenIdConnectEvents
+            {
+                OnRedirectToIdentityProviderForSignOut = (context) =>
+                {
+                    var logoutUri = $"https://{Configuration["Auth0:Domain"]}/v2/logout?client_id={Configuration["Auth0:ClientId"]}";
+
+                    var postLogoutUri = context.Properties.RedirectUri;
+                    if (!string.IsNullOrEmpty(postLogoutUri))
+                    {
+                        if (postLogoutUri.StartsWith("/"))
+                        {
+                            var request = context.Request;
+                            postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                        }
+                        logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+                    }
+
+                    context.Response.Redirect(logoutUri);
+                    context.HandleResponse();
+
+                    return Task.CompletedTask;
+                }
+            };
         }
     }
 }
